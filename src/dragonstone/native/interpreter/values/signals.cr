775515556b5324032d373ab4830235a29ff7fca0
Vallereya <v@vallereya.com>
2026-08-23 22:18:37 -0400

    Update signals.cr
    add return targeting to unwind the stack for non-local block returns.

--- src/dragonstone/native/interpreter/values/signals.cr
+++ src/dragonstone/native/interpreter/values/signals.cr
@@ -1,9 +1,15 @@
 module Dragonstone
     class ReturnValue < Exception
         getter value : RuntimeValue?
+        getter target : UInt64?
 
-        def initialize(@value : RuntimeValue?)
+        def initialize(@value : RuntimeValue?, @target : UInt64? = nil)
             super()
+        end
+
+        def claimed_by?(frame_id : UInt64) : Bool
+            target = @target
+            target.nil? || target == frame_id
         end
     end
 
