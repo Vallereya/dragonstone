978d1ad190a063a5fcbe3737a0e3ddfe39f2e66c
Vallereya <v@vallereya.com>
2026-09-02 18:46:48 -0400

    updated regression scripts to mirror bootstrap

--- bin/scripts/regression/check_all.ps1
+++ bin/scripts/regression/check_all.ps1
@@ -1,9 +1,14 @@
 $ErrorActionPreference = "Stop"
 
-# Always run relative to this script's folder, this
-# will run check_llvm, check_c, and check_run together.
-# Example:
-#   `powershell .\bin\scripts\regression\check_all.ps1`
+<#
+    Always run relative to this script's folder, this will 
+    run check_llvm and check_run together.
+
+    Example:
+        `powershell .\bin\scripts\regression\check_all.ps1`
+
+#>
+
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 Set-Location $here
 
--- bin/scripts/regression/check_all.sh
+++ bin/scripts/regression/check_all.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Always run relative to this script's folder, this
-# will run check_llvm, check_c, and check_run together.
+# Always run relative to this script's folder, this will
+# run check_llvm, check_c, and check_run together.
+# 
 # Example:
 #   `bash ./bin/scripts/regression/check_all.sh`
+# 
+
 HERE="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
 "$HERE/check_run.sh"
